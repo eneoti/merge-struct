9008026421a96a9a12fb87bbdb968408fbdfda01
nam <namddt@gmail.com>
2018-12-24 10:32:51 +0700

    refine base while merge

--- merge.go
+++ merge.go
@@ -1,6 +1,8 @@
 package merp
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/fatih/structs"
 	"github.com/mitchellh/mapstructure"
 	"github.com/pkg/errors"
@@ -10,6 +12,7 @@ import (
 // MergeOverwrite overwrite map
 func MergeOverwrite(to, from, dst interface{}) error {
 	var toMap map[string]interface{}
+	var result map[string]interface{}
 	var fromMap map[string]interface{}
 	kindTo := reflect.ValueOf(to)
 	kindFrom := reflect.ValueOf(from)
@@ -24,14 +27,66 @@ func MergeOverwrite(to, from, dst interface{}) error {
 		fromMap = structs.Map(from)
 	}
 	for k, v := range fromMap {
-		_, ok := toMap[k]
-		if !ok {
-			continue
-		}
-		toMap[k] = v
+		mapBase(k, v, &fromMap)
+		toMap[k] = fromMap[k]
 	}
+	_, ok := toMap["Base"]
+	if ok {
+		delete(toMap, "Base")
+	}
+
+	conv := ConventionalMarshaller{Value: toMap}
+	b, err := conv.MarshalJSON()
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(b, &result)
+	// fmt.Println(result)
+	// fmt.Println(toMap)
 	if err := mapstructure.Decode(toMap, dst); err != nil {
 		return errors.Wrap(err, "failed to decode")
 	}
 	return nil
+}
+func mapBase(key string, value interface{}, org *(map[string]interface{})) {
+	result := *org
+	if key == "Base" {
+		temp := value.(map[string]interface{})
+		for k, v := range temp {
+			result[k] = v
+		}
+		delete(result, "Base")
+		return
+	}
+	valueRf := reflect.ValueOf(value)
+	kind := valueRf.Kind()
+	switch kind {
+	case reflect.Map:
+		temp := value.(map[string]interface{})
+		for k, v := range temp {
+			mapBase(k, v, &temp)
+		}
+	case reflect.Slice:
+		temp := value.([]interface{})
+		for _, v := range temp {
+			temp2 := v.(map[string]interface{})
+			for k1, v1 := range temp2 {
+				mapBase(k1, v1, &temp2)
+			}
+		}
+	case reflect.Ptr:
+		if !valueRf.IsNil() {
+			temp := valueRf.Elem()
+			// fmt.Println(reflect.TypeOf(value))
+			fmt.Println(temp)
+			// temp = temp.(map[string]interface{})
+			// for k, v := range temp {
+			// 	mapBase(k, v, &temp)
+			// }
+			// value = &temp
+		}
+	case reflect.String:
+		result[key] = value
+	}
+	org = &result
 }
